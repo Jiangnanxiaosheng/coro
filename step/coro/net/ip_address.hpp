878b6aa781ec3a3411c15c5b40873327e4f4ebfc
jiangnanxiaosheng <2996965087@qq.com>
2025-02-27 16:43:11 +0800

    删除Domain枚举类，移除对Ipv6的支持

--- step/coro/net/ip_address.hpp
+++ step/coro/net/ip_address.hpp
@@ -9,68 +9,39 @@
 #include <arpa/inet.h>
 #include <array>
 #include <span>
-#include <iostream>
 #include <string>
 #include <cstring>
-
-enum class Domain {
-    IpV4 = AF_INET,
-    IpV6 = AF_INET6,
-};
-
-static std::string Domain_Ipv4{"ipv4"};
-static std::string Domain_Ipv6{"ipv6"};
-
-inline std::string& to_string(Domain domain) {
-    switch (domain) {
-        case Domain::IpV4:
-            return Domain_Ipv4;
-        case Domain::IpV6:
-            return Domain_Ipv6;
-    }
-    throw std::runtime_error {"to_string(Domain) unknown domain"};
-}
+#include <stdexcept>
 
 
 class IpAddress {
 public:
     static constexpr size_t ipv4_len {4};
-    static constexpr size_t ipv6_len {16};
 
     IpAddress() = default;
-    IpAddress(std::span<const uint8_t> binary_address, Domain domain = Domain::IpV4) : m_domain(domain) {
-        if (m_domain == Domain::IpV4 && binary_address.size() > ipv4_len) {
-            throw std::runtime_error{"coro::net::IpAddress provided binary ip address is too long"};
-        } else if (binary_address.size() > ipv6_len) {
-            throw std::runtime_error{"coro::net::IpAddress provided binary ip address is too long"};
+    IpAddress(std::span<const uint8_t> binary_address) {
+        if (binary_address.size() > ipv4_len) {
+            throw std::runtime_error{"coro::net::IpAddress provided binary ipv4 address is too long"};
         }
         std::copy(binary_address.begin(), binary_address.end(), m_data.begin());
     }
 
-    Domain domain() const {
-        return m_domain;
-    }
 
-    static auto from_string(const std::string& address, Domain domain = Domain::IpV4) {
+    static auto from_string(const std::string& ip) {
         IpAddress addr {};
-        addr.m_domain = domain;
 
-        auto success = inet_pton(static_cast<int>(addr.m_domain), address.data(), addr.m_data.data());
+        auto success = inet_pton(AF_INET, ip.data(), addr.m_data.data());
         if (success != 1) {
-            throw std::runtime_error {"coro::net::IpAdress faild to convert from string"};
+            throw std::runtime_error {"coro::net::IpAddress failed to convert from string"};
         }
         return addr;
     }
 
     std::string to_string() const {
         std::string output;
-        if (m_domain == Domain::IpV4) {
-            output.resize(INET_ADDRSTRLEN, '\0');
-        } else {
-            output.resize(INET6_ADDRSTRLEN, '\0');
-        }
+        output.resize(INET_ADDRSTRLEN, '\0');
 
-        auto success = inet_ntop(static_cast<int>(m_domain), m_data.data(), output.data(), output.length());
+        auto success = inet_ntop(AF_INET, m_data.data(), output.data(), output.length());
         if (success != nullptr) {
             auto len = strnlen(success, output.length());
             output.resize(len);
@@ -81,9 +52,12 @@ public:
         return output;
     }
 
+    std::span<const uint8_t> data() const {
+        return std::span<const uint8_t>{m_data.data(), ipv4_len};
+    }
+
 private:
-    std::array<uint8_t, ipv6_len> m_data{};
-    Domain m_domain {Domain::IpV4};
+    std::array<uint8_t, ipv4_len> m_data{};
 };
 
 #endif //CORO_IP_ADDRESS_HPP
