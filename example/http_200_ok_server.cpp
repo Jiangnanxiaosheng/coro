a1ed075dcb7b47cffd35942716523504e35ad742
jiangnanxiaosheng <2996965087@qq.com>
2025-09-29 12:12:26 +0800

    http_200_ok_server

--- example/http_200_ok_server.cpp
+++ example/http_200_ok_server.cpp
@@ -1,16 +1,15 @@
+#include <coro/coro.hpp>
 #include <iostream>
 #include <memory>
-
-#include <coro/coro.hpp>
 
 using namespace coro;
 using namespace coro::net;
 
 int main() {
-    auto http_200_ok_server = [] (std::shared_ptr<IoScheduler> scheduler) -> Task<> {
-        auto connection_task = [] (tcp::Client client) -> Task<> {
+    auto http_200_ok_server = [](std::shared_ptr<IoScheduler> scheduler) -> Task<> {
+        auto connection_task = [](tcp::Client client) -> Task<> {
             std::string response =
-                    R"(HTTP/1.1 200 OK
+                R"(HTTP/1.1 200 OK
 Content-Length: 0
 Connection: keep-alive
 
@@ -20,51 +19,47 @@ Connection: keep-alive
                 co_await client.poll(PollOp::Read);
                 auto [rstatus, rspan] = client.recv(buf);
                 switch (rstatus) {
-                    case RecvStatus::Ok:
-                        co_await client.poll(PollOp::Write);
-                        client.send(std::span<const char>{response});
-                        break;
-                    case RecvStatus::WouldBlock:
-                        break;
-                    case RecvStatus::Closed:
-                    default:
-                        co_return;
+                case RecvStatus::Ok:
+                    co_await client.poll(PollOp::Write);
+                    client.send(std::span<const char>{response});
+                    break;
+                case RecvStatus::WouldBlock:
+                    break;
+                case RecvStatus::Closed:
+                default:
+                    co_return;
                 }
             }
         };
 
         co_await scheduler->schedule();
 
-        tcp::Server server {scheduler, tcp::Server::LocalEndPoint{.port = 8080}};
+        tcp::Server server{scheduler, tcp::Server::LocalEndPoint{.port = 8080}};
         while (true) {
-            auto pstatus =  co_await server.poll();
+            auto pstatus = co_await server.poll();
             switch (pstatus) {
-                case PollStatus::Event:
-                {
-                    auto client = server.accept();
-                    if (client.socket().is_valid()) {
-                        scheduler->spawn(connection_task(std::move(client)));
-                    }
+            case PollStatus::Event: {
+                auto client = server.accept();
+                if (client.socket().is_valid()) {
+                    scheduler->spawn(connection_task(std::move(client)));
                 }
-                    break;
-                case PollStatus::Error:
-                case PollStatus::Closed:
-                case PollStatus::Timeout:
-                default:
-                    co_return;
+            } break;
+            case PollStatus::Error:
+            case PollStatus::Closed:
+            case PollStatus::Timeout:
+            default:
+                co_return;
             }
         }
         co_return;
     };
 
-
     std::vector<Task<>> workers{};
     for (int i = 0; i < 16; ++i) {
-        auto scheduler= IoScheduler::make_shared(IoScheduler::Options{.execution_strategy = io_exec_thread_pool});
+        auto scheduler = IoScheduler::make_shared(
+            IoScheduler::Options{.execution_strategy = io_exec_thread_inline});
         workers.push_back(http_200_ok_server(scheduler));
     }
 
-
     sync_wait(when_all(std::move(workers)));
-
 }
