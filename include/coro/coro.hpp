6d91ce229ed114e7921276e6d9e9dd1e5c3c9926
jiangnanxiaosheng <2996965087@qq.com>
2025-09-29 12:11:34 +0800

    修改头文件包含

--- include/coro/coro.hpp
+++ include/coro/coro.hpp
@@ -7,23 +7,21 @@
 #ifdef NETWORKING
 
 #include "coro/net/connect_status.hpp"
+#include "coro/net/ip_address.hpp"
 #include "coro/net/recv_status.hpp"
 #include "coro/net/send_status.hpp"
-#include "coro/net/ip_address.hpp"
 #include "coro/net/socket.hpp"
 #include "coro/net/tcp/client.hpp"
-#include "coro/net/tcp/server.hpp"
-
 #include "coro/net/tcp/http/http_server.hpp"
+#include "coro/net/tcp/server.hpp"
 
 #endif
 
+#include "coro/io_scheduler.hpp"
+#include "coro/poll.hpp"
+#include "coro/sync_wait.hpp"
 #include "coro/task.hpp"
 #include "coro/thread_pool.hpp"
-#include "coro/sync_wait.hpp"
 #include "coro/when_all.hpp"
-#include "coro/poll.hpp"
-#include "coro/io_scheduler.hpp"
 
-
-#endif //CORO_CORO_HPP
+#endif  // CORO_CORO_HPP
